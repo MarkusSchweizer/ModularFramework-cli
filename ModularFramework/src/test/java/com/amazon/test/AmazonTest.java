6c27ca39145984d1f2fec65a777cc8472bff547e
MarkusSchweizer <71660393+MarkusSchweizer@users.noreply.github.com>
2020-09-24 14:40:22 +0200

    Update JaveTest to Apple the second

--- ModularFramework/src/test/java/com/amazon/test/AmazonTest.java
+++ ModularFramework/src/test/java/com/amazon/test/AmazonTest.java
@@ -37,7 +37,7 @@ CommonDriver cmnDriver;
 	
 	@Test
 	public void searchProduct() throws Exception{
-		String product = "Apple mac pro laptop";
+		String product = "Apple mac pro laptop the second";
 		String category = "Electronics";
 		
 		homepage.searchProduct(product, category);
